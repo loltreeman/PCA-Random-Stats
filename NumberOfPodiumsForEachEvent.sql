9ba3f7f188df482243bbce883ae2b242507a2c93
trix <christian.naguio@student.ateneo.edu>
2024-12-30 00:30:09 +0800

    Update NumberOfPodiumsForEachEvent.sql

--- NumberOfPodiumsForEachEvent.sql
+++ NumberOfPodiumsForEachEvent.sql
@@ -18,3 +18,24 @@ WHERE personid = ':WCA_ID'
     AND best > 0
 GROUP BY e.name, r.PersonName
 ORDER BY e.rank, r.PersonName;
+
+
+-- If you want the Gold, Silver, Bronze breakdown
+
+SELECT
+    r.PersonName,
+    e.name AS Event,
+    COUNT(CASE WHEN r.pos = 1 THEN 1 ELSE NULL END) AS Gold,
+    COUNT(CASE WHEN r.pos = 2 THEN 1 ELSE NULL END) AS Silver,
+    COUNT(CASE WHEN r.pos = 3 THEN 1 ELSE NULL END) AS Bronze
+FROM Results r
+    INNER JOIN Competitions c
+        ON r.competitionid = c.id
+    INNER JOIN Events e
+        ON r.eventid = e.id
+WHERE personid = ':WCA_ID'
+    AND roundtypeid IN ('c', 'f')
+    AND pos <= 3
+    AND best > 0
+GROUP BY e.name, r.PersonName
+ORDER BY e.rank, r.PersonName;
