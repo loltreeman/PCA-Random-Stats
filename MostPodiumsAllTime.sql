ab89bc70c27c6ffbb13af633ae7dd3bee081397a
trix <christian.naguio@student.ateneo.edu>
2024-12-30 17:37:48 +0800

    Create MostPodiumsAllTime.sql

---
+++ MostPodiumsAllTime.sql
@@ -0,0 +1,5 @@
+SELECT personName, SUM(IF(pos=1, 1, 0)) golds, SUM(IF(pos=2, 1, 0)) silvers, SUM(IF(pos=3, 1, 0)) bronzes, COUNT(*) total
+FROM Results
+WHERE countryId = 'Philippines' AND roundTypeId IN ('c','f') AND best > 0 AND pos <= 3
+GROUP BY personName
+ORDER BY COUNT(*) DESC
